e8612432d714bb2561858a44e23fabbf2a9bb344
Łukasz Grząśko <lukasz.grzasko@hotmail.com>
2017-11-26 01:11:06 +0100

    validating matrices inputs

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,6 @@
 (function ($) {
 
+    /*Funkcje do generowania macierzy*/
     let methodRadios = $('input[name="method"]'),
         matriceSizer2 = $('#matrice-sizer-2'),
         methodDeterminant = null;
@@ -9,7 +10,7 @@
         secondMatrice = $('.matrices-wrapper table')[1];
 
 
-        if(radio.val() == 1) {
+        if(radio.val() == 'determinant') {
             matriceSizer2.slideUp();
             secondMatrice && $(secondMatrice).fadeOut();
             methodDeterminant = true;
@@ -29,17 +30,17 @@
         matrice2Col = $('#matrice2Col');
 
     matriceInputs.on('keyup', function () {
-        var input = $(this);
+        let input = $(this);
         input.val() > 10 && input.val('10');
 
         if(!methodDeterminant) {
 
-            if( matrice1Row.val() && matrice1Col.val() && matrice2Row.val() && matrice2Col.val() ) {
-                generateMatrices( matrice1Row.val(), matrice1Col.val(), matrice2Row.val(), matrice2Col.val() );
+            if(matrice1Row.val() && matrice1Col.val() && matrice2Row.val() && matrice2Col.val()) {
+                generateMatrices(matrice1Row.val(), matrice1Col.val(), matrice2Row.val(), matrice2Col.val());
             }
 
-        } else if( methodDeterminant && matrice1Row.val() && matrice1Col.val() ) {
-            generateMatrices( matrice1Row.val(), matrice1Col.val() );
+        } else if(methodDeterminant && matrice1Row.val() && matrice1Col.val()) {
+            generateMatrices(matrice1Row.val(), matrice1Col.val());
         }
     });
 
@@ -78,5 +79,86 @@
 
         matricesWrapper.append(newMatrice);
     };
+
+    /*Funkcje do pobierania danych z macierzy*/
+    let optionsForm = $('#options-form');
+
+    generateEachMatrice(2, 3)
+
+    optionsForm.on('submit', function (event) {
+        event.preventDefault();
+
+        let firstMatriceArr = [],
+			secondMatriceArr = [],
+			leftMatrice = $($('.matrices-wrapper table')[0]),
+			rightMatrice = $($('.matrices-wrapper table')[1]);
+
+
+        /* Umieszczanie wartoście w tablicy z LEWEJ macierzy*/
+        let leftMatriceRows = leftMatrice.find($('tr'));
+
+        leftMatriceRows.each(function (index, eachRow) {
+
+			eachRow = $(eachRow);
+
+			let rowArray = [],
+				fields = eachRow.find('td');
+
+			fields.each(function (index, eachField) {
+
+				let input = $(eachField).find('input');
+
+				input.val() && rowArray.push(input.val());
+
+			});
+
+			firstMatriceArr.push(rowArray);
+		});
+
+
+        /* Umieszczanie wartoście w tablicy z PRAWEJ macierzy*/
+        if(!methodDeterminant) {
+            let rightMatriceRows = rightMatrice.find($('tr'));
+
+            rightMatriceRows.each(function (index, eachRow) {
+
+                eachRow = $(eachRow);
+
+                let rowArray = [],
+                    fields = eachRow.find('td');
+
+                fields.each(function (index, eachField) {
+
+                    let input = $(eachField).find('input');
+
+                    input.val() && rowArray.push(input.val());
+
+                });
+
+                secondMatriceArr.push(rowArray);
+            });
+        }
+
+        /*Funkcje liczące działania na macierzach*/
+        let checkedMethod = $('input[name="method"]:checked').val();
+
+        if(checkedMethod == 'addition' && matrice1Row.val() == matrice2Row.val() && matrice1Col.val() == matrice2Col.val()) {
+
+            for(var i = 0; i < matrice2Row.val(); i++) {
+
+                if(secondMatriceArr[i].length == matrice2Col.val()) {
+                    console.log('ok');
+                } else {
+                    console.log('uzupelnij pola');
+                }
+
+            }
+
+            console.log(secondMatriceArr);
+
+        } else {
+            console.log('Dodawanie mogę wykonac tylko wtedy gdy macierze maja takie same wymiary');
+        }
+    });
 
 })(jQuery);
